065f3dbc219d8d0e7c2ec483104fe9818873012e
Steven De Toni <steven@waikato.ac.nz>
2022-08-31 10:26:57 +1200

    Updated to vault 1.11 Added ldap/oidc user/group account creation

---
+++ src/recsearch/js_libs/customisation.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Add customisation code here, for example : 
+// 
+//Set default Domain name for Domain group add/remove
+//var gdDomainName = "MyDomain"
+// 
+//Add custom completion message for Domain group add 
+//function gdDomainAddedUsrGrpMsg (domainName, mountName)
+//{
+//    return "Completed User/Group build in domain " + domainName + " at mount point " + mountPoint + ".\n\n";
+//}
