95a761595a5c395c76c980a66c9eb2a1f92136c3
Steven De Toni <steven@waikato.ac.nz>
2022-09-13 12:03:58 +1200

    Added deeplink goto secret path url function

--- src/recsearch/js_libs/gen_funcs.js
+++ src/recsearch/js_libs/gen_funcs.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function gfGetParameterByName(name, url = window.location.href) 
+{
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 function gfRefreshIFramePage ()
 {
     document.getElementById(FrameID).contentDocument.location.reload()
